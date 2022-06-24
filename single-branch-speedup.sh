fbc91e9ff4d8d566e14f51746a7e0aaa152af0a9
Jean Fontena <diegofontena@gmail.com>
2022-06-24 11:04:35 +0200

    Add script to trace speedup curves for a single branch using multiple sleeping durations

---
+++ single-branch-speedup.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+set -e
+
+SLEEPING_THRESHOLDS=(10 50 100 500 1000 10000 100000)
+WAITING_TIME_MULTIPLIER=2
+
+BENCHES_DIR=`pwd`
+RAYON_PATH=$BENCHES_DIR/../rayon-fork
+RUNS=1
+
+bench () {  
+  CORES=`nproc --all`
+  for threads in $(seq 1 $CORES);
+  do
+    for st in ${SLEEPING_THRESHOLDS[@]}; do
+      echo $threads $st $WAITING_TIME_MULTIPLIER target/release/benchmarks speedup-$threads-threads-$st-us.csv
+    done
+  done
+}
+
+# run threads sleeping_threshold multiplier binary_name output_filename
+run () {
+  local threads=$1
+  local sleeping_threshold=$2
+  local multiplying_factor=$3
+  local binary_name=$4
+  local output_filename=$5
+
+  echo "Running $binary_name with $threads threads, st = $sleeping_threshold, tm = $multiplying_factor [$output_filename]"
+  sudo RAYON_NUM_THREADS=$threads \
+       SLEEPING_THRESHOLD_US=$sleeping_threshold \
+       WAITING_TIME_MULTIPLIER=$multiplying_factor perf stat ./$binary_name --runs $RUNS --output-filename $output_filename
+}
+
+bench
+
