93bbd5766153c041b40905561be24c42b67f7488
Jean Fontena <diegofontena@gmail.com>
2022-07-20 10:51:14 +0200

    Add script to perform speedup experiments using the number of threads (and not the cores) without fixing the frequency of the cores

---
+++ scripts/speedup.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+set -e
+
+# variables
+RAYON_PATH="/home/jsilvafontena/lig/rayon-fork"
+BENCHMARKS_PATH="/home/jsilvafontena/lig/benchmarks-rs"
+RAYON_BRANCH="master"
+RUNS=50
+
+build () {
+  cd $RAYON_PATH && git checkout $RAYON_BRANCH
+  cd $BENCHMARKS_PATH && cargo clean && cargo build --release
+}
+
+bench () {  
+  THREADS=`nproc --all`
+
+  for nb_threads in $(seq 1 $THREADS);
+  do
+    run $nb_threads target/release/benchmarks output/speedup-$RAYON_BRANCH-$threads-threads.csv
+  done
+}
+
+# run threads binary_name output_filename
+run () {
+  local threads=$1
+  local binary_name=$2
+  local output_filename=$3
+
+  echo "Running $binary_name with $threads threads [$output_filename]"
+  sudo RAYON_NUM_THREADS=$threads ./$binary_name --runs $RUNS --output-filename $output_filename
+}
+
+build
+bench
